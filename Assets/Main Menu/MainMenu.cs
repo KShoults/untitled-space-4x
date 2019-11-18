da0f8aa69e2727782fe116763060cea28575bfd3
Kilatic <kobishoults@gmail.com>
2019-11-18 11:53:57 -0600

    Create main menu scene with title, start, and quit game buttons.

---
+++ Assets/Main Menu/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Main View");
+        Time.timeScale = 1.0f;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
