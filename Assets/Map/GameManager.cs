e4f7248a34480c226d1403742b5c93ed9f7aaa74
Kilatic <kobishoults@gmail.com>
2019-11-18 19:58:39 -0600

    Implemented sector generation to generate clusters

---
+++ Assets/Map/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Galaxy generation default settings
+    public int numClusters, numSystemsAvg, numSystemsVar, sizePlanetsAvg, sizePlanetsVar, clusterSpacing;
+    public Sector sector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Create the sector using perlin noise maps
+        sector.GenerateSector(numClusters, clusterSpacing, numSystemsAvg, numSystemsVar, sizePlanetsAvg, sizePlanetsVar);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Map/Sector/Sector.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sector : MonoBehaviour
+{
+    public GameObject ClusterPrefab;
+    public List<GameObject> clusters;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    /* Generate clusters and have them generate their systems
+    /  numClusters: The guaranteed number of clusters in the sector
+    /  numSystemsAvg: The average number of systems in each cluster
+    /  numSystemsVar: The variance in the number of systems in each cluster
+    /  sizePlanetsAvg: The average total aggregate size of planets in each system
+    /  sizePlanetsVar: The variance in the total aggregate size of planets in each system
+    */
+    public void GenerateSector(int numClusters, int clusterSpacing, int numSystemsAvg, float numSystemsVar, int sizePlanetsAvg, int sizePlanetsVar)
+    {
+        // Generate an unweighted noise map with a size based on the number of clusters
+        int mapWidth = numClusters * 50;
+        float[,] weightMap = new float[mapWidth, mapWidth];
+        for (int i = 0; i < mapWidth; i++)
+        {
+            for (int j = 0; j < mapWidth; j++)
+            {
+                weightMap[i,j] = 1f;
+            }
+        }
+        float[,] noiseMap = NoiseMapGenerator.GenerateNoiseMap(weightMap, 1f);
+
+        // Log the noise map
+        /*for (int i = 0; i < mapWidth; i++)
+        {
+            string row = "";
+            for (int j = 0; j < mapWidth; j ++)
+            {
+                row += Mathf.Floor(noiseMap[i,j] * 10);
+            }
+            Debug.Log(row);
+        }*/
+
+        clusters = new List<GameObject>();
+        // For each cluster, find the largest point in the noise map and place a cluster
+        for (int cluster = 0; cluster < numClusters; cluster++)
+        {
+            float bestValue = 0;
+            int bestX = 0, bestY = 0;
+
+            for (int i = 0; i < mapWidth; i++)
+            {
+                for (int j = 0; j < mapWidth; j++)
+                {
+                    if (noiseMap[i,j] > bestValue)
+                    {
+                        bestX = i;
+                        bestY = j;
+                        bestValue = noiseMap[i,j];
+                    }
+                }
+            }
+
+            // Create and add the cluster
+            clusters.Add(GameObject.Instantiate(ClusterPrefab, new Vector3(bestX, bestY, 0), Quaternion.identity, transform));
+
+            // Prevent other clusters from spawning too close
+            for (int i = 0-clusterSpacing; i < clusterSpacing; i++)
+            {
+                for (int j = 0-clusterSpacing; j < clusterSpacing; j++)
+                {
+                    int targetX = bestX + i, targetY = bestY + j;
+                    if (targetX >= 0 && targetX < mapWidth &&
+                        targetY >= 0 && targetY < mapWidth)
+                    {
+                        noiseMap[targetX, targetY] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
