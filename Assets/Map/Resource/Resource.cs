3345ab424bdc12495e22dcc61e1b59159bc997ff
Kilatic <kobishoults@gmail.com>
2019-11-29 20:52:49 -0600

    Implement resource panel UI to show the resources on a selected planet.

--- Assets/Map/Resource/Resource.cs
+++ Assets/Map/Resource/Resource.cs
@@ -12,3 +12,25 @@ public enum Resource
     MilitaryGoods,
     ShipParts
 }
+
+public static class ResourceUtil
+{
+    private static readonly Dictionary<Resource, string> _resourceString = new Dictionary<Resource, string>
+    {
+        {Resource.Energy, "Energy"},
+        {Resource.Water, "Water"},
+        {Resource.Food, "Food"},
+        {Resource.Minerals, "Minerals"},
+        {Resource.CivilianGoods, "Civilian Industry"},
+        {Resource.MilitaryGoods, "Military Industry"},
+        {Resource.ShipParts, "Shipyards"}
+    };
+
+    public static Dictionary<Resource, string> ResourceString
+    {
+        get
+        {
+            return _resourceString;
+        }
+    }
+}
