bf935c5c65183fb2225fec70096e4086e974c0d8
Kilatic <kobishoults@gmail.com>
2020-01-12 15:52:33 -0600

    Implemented cluster overlay to show system names and the presence of player industries in that system

--- Assets/Map/StarSystem/StarSystem.cs
+++ Assets/Map/StarSystem/StarSystem.cs
@@ -140,17 +140,6 @@ public class StarSystem : MonoBehaviour
         }
     }
 
-    // Alert the InputManager when this is hovered over
-    void OnMouseEnter()
-    {
-        Camera.main.GetComponent<InputManager>().HoverEnter(this);
-    }
-
-    void OnMouseExit()
-    {
-        Camera.main.GetComponent<InputManager>().HoverExit(this);
-    }
-
     void OnMouseDown()
     {
         if (Input.GetMouseButton(0))
--- Assets/UI/Overlay/Cluster/ClusterSystemDevelopmentOverlay.cs
+++ Assets/UI/Overlay/Cluster/ClusterSystemDevelopmentOverlay.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ClusterSystemDevelopmentOverlay : MonoBehaviour
 {
     public StarSystem starSystem;
+    public TextMeshProUGUI SystemNameText;
 
     void OnEnable()
     {
@@ -12,6 +14,27 @@ public class ClusterSystemDevelopmentOverlay : MonoBehaviour
         {
             gameObject.SetActive(false);
             return;
+        }
+
+        SystemNameText.text = starSystem.starSystemShortName;
+
+        bool isDeveloped = false;
+        // Check for player allocation in this system
+        foreach (Planet p in starSystem.planets)
+        {
+            if (p.industries.Count > 0)
+            {
+                isDeveloped = true;
+                break;
+            }
+        }
+        if (isDeveloped)
+        {
+            SystemNameText.color = Color.red;
+        }
+        else
+        {
+            SystemNameText.color = Color.white;
         }
     }
 }
