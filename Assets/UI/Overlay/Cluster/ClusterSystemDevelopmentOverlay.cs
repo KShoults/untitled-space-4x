73cfea4ddca3f447ea6fad279b27bcb16f100bf0
Kilatic <kobishoults@gmail.com>
2020-01-12 15:04:19 -0600

    Add an empty cluster development overlay

---
+++ Assets/UI/Overlay/Cluster/ClusterSystemDevelopmentOverlay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClusterSystemDevelopmentOverlay : MonoBehaviour
+{
+    public StarSystem starSystem;
+
+    void OnEnable()
+    {
+        if (starSystem == null)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+    }
+}
