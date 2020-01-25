223d0dedd79385681c053e9e330c85c7feb9ae9e
Kilatic <kobishoults@gmail.com>
2020-01-25 16:20:15 -0600

    Add unit tests to Producer.EstimateCost

--- Assets/Tests/ContractSystem/ProducerTestSuite.cs
+++ Assets/Tests/ContractSystem/ProducerTestSuite.cs
@@ -28,6 +28,48 @@ namespace Tests
             }
         }
 
+        [TestCase(new float[] {1}, new float[] {1},
+                  new float[] {1}, new float[] {2},
+                  1.5f, ExpectedResult=2 / 1.5f)]
+        [TestCase(new float[] {1, 1.5f, 2}, new float[] {.5f, .75f, 1},
+                  new float[] {1}, new float[] {2},
+                  2, ExpectedResult=1.25f / 2)]
+        [TestCase(new float[] {1}, new float[] {1},
+                  new float[] {1.5f, 1, 2}, new float[] {.75f, .5f, 1},
+                  3.5f, ExpectedResult=2.625f / 3.5f)]
+        [TestCase(new float[] {1}, new float[] {1},
+                  new float[] {1}, new float[] {2},
+                  3, ExpectedResult=3 / 3)]
+        public float EstimateCostReturnsCorrectCost(float[] imports, float[] importCosts, float[] suppliers, float[] supplierCosts, float targetCapacity)
+        {
+            // Make an empty contract system
+            GameManager.contractSystem = new ContractSystem();
+            
+            // Create our producer
+            ProducerMock producer = new ProducerMock(Resource.Minerals);
+
+            // Add the imports
+            for (int i = 0; i < imports.Length; i++)
+            {
+                producer.contractTerminal.importContracts[Resource.Energy].Add(new Contract(Resource.Energy, 0, imports[i], importCosts[i], null, producer.contractTerminal));
+            }
+
+            // Add the suppliers
+            if (!producer.contractTerminal.suppliers.ContainsKey(Resource.Energy))
+            {
+                producer.contractTerminal.suppliers.Add(Resource.Energy, GameManager.contractSystem.FindSuppliers(Resource.Energy));
+            }
+            for (int i = 0; i < suppliers.Length; i++)
+            {
+                producer.contractTerminal.suppliers[Resource.Energy].Add(new System.Tuple<ContractTerminal, float, float>(
+                                                                        new ContractTerminal(null, Resource.Energy, new List<Resource>()),
+                                                                        suppliers[i], supplierCosts[i]));
+            }
+
+            // Call EstimateCost
+            return producer.EstimateCost(targetCapacity)[Resource.Minerals];
+        }
+
         [TestCase(new float[] {0}, new float[] {1},
                   ExpectedResult=1)]
         [TestCase(new float[] {50, 0}, new float[] {1.5f},
